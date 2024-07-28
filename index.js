22e0ae8268787be0394634fdc72d83da64ccbe9c
dev-vinicius-lima <viniciuslimaes@hotmail.com>
2024-07-28 19:58:52 -0400

    feat: :tada: started project agenda system

---
+++ index.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import mongoose from 'mongoose';
+const app = express();
+
+mongoose.connect(
+  'mongodb+srv://viniciusfhlima:NLPPY13cqMb6fZ3f@agendasystem.twiuj5a.mongodb.net/?retryWrites=true&w=majority&appName=agendaSystem',
+);
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+  console.log('Database Connected successfully!');
+});
+
+app.use(express.json());
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(3333, () => {
+  console.log('executing on port 3333!');
+});
